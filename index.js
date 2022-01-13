424841a71f51ce50cd6f5cfc4ba9bc396b935d4f
Carlos Iván Poclín Meza <carlos.poclin@unmsm.edu.pe>
2022-01-13 15:26:45 -0500

    chore(env): ad

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const controller = require('./controllers/posts.controller')
 
-mongoose
-  .connect(
-    'mongodb+srv://cpoclin:cpoclin@main.aakwp.mongodb.net/test?retryWrites=true&w=majority',
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    const app = express()
+require('dotenv').config()
 
-    app.use(cors())
-    app.use(express.json())
-    app.use('/', controller)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+  const app = express()
 
-    app.listen(5000, () => {
-      console.log('Server has started!')
-    })
+  app.use(cors())
+  app.use(express.json())
+  app.use('/', controller)
+
+  app.listen(5000, () => {
+    console.log('Server has started!')
   })
+})
